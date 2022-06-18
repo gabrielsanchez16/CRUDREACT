564f9e45e188fc750837917a6f5d2d0093e2d627
gabriel sanchez <gabrielpelota.8@gmail.com>
2022-06-18 00:11:06 -0500

    changeicons

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -1,4 +1,5 @@
-
+import { FaBirthdayCake, FaPencilAlt } from "react-icons/fa";
+import {BsTrashFill} from 'react-icons/bs'
 
 const CardUser = ({user}) => {
     console.log(user)
@@ -7,11 +8,11 @@ const CardUser = ({user}) => {
         <h2>{user.first_name} {user.last_name}</h2>
         <ul>
             <li className="email">{user.email}</li>
-            <li className="birthday">{user.birthday}</li>
+            <li className="birthday"><FaBirthdayCake/> {user.birthday}</li>
         </ul>
         <div className="container-buttons">
-            <button className="delete">Actualizar</button>
-            <button className="update">Delete</button>
+            <button className="delete"><BsTrashFill/></button>
+            <button className="update"><FaPencilAlt/></button>
         </div>
         
     </article>
